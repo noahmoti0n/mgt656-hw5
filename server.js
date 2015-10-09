63f3209f17f75c719f606af3cd71f51c21f8a069
noahmoti0n <noah.aptekar@gmail.com>
2015-10-09 18:39:23 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -5,4 +5,9 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
+app.get('/nickname', function(request, response){
+    response.send('shiny-trout');
+});
+
+
 app.listen(process.env.PORT || 4000);
